e4f01dec5ce211ae49a687846e009e66ab9773af
Lucas Bryan <lucasbryan0217@gmail.com>
2022-06-30 22:24:01 -0300

    feat: componentizate head, and cards pages

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "recipes/pages/home.html")
+
+
+def recipe(request, id_recipe):
+    return render(request, "recipes/pages/recipe-view.html")
